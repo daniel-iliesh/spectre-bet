f58f88c1268fb19e73a67108167f94a1ae672b4b
Daniel Ilies <iliesh.daniel@gmail.com>
2025-07-09 13:34:07 +0300

    use user data taken from twa

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -3,8 +3,16 @@
     <v-app-bar-title>
       <div class="d-flex justify-space-between w-100">
         <v-avatar class="pa-2">
-          <span class="text-h6">RO</span>
+          <span class="text-h6">
+            {{
+              user?.firstName?.charAt(0) ||
+              "J" + user?.lastName?.charAt(0) ||
+              "D"
+            }}
+          </span>
+          <!-- <v-img :src="fakeUser.photo_url" /> -->
         </v-avatar>
+        <span v-if="user">Welcome, {{ user.firstName }}!</span>
         <v-btn class="mr-6 bg-white"> 0 USD </v-btn>
       </div>
     </v-app-bar-title>
@@ -13,7 +21,7 @@
 
 <script lang="ts" setup>
 import WebApp from "@twa-dev/sdk";
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 
 interface UserData {
   id: number;
@@ -23,7 +31,9 @@ interface UserData {
   photo_url?: string;
 }
 
+const user = ref<UserData | null>(null);
+
 onMounted(() => {
-  console.log(WebApp.initDataUnsafe.user as UserData);
+  user.value = WebApp.initDataUnsafe.user as UserData;
 });
 </script>
